413ab73bd03c5e6b2608475ee068ceab049937a4
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2025-06-18 21:58:01 -0300

    feat: UserService: CRUD User

---
+++ src/services/UserService.ts
@@ -0,0 +1,50 @@
+import { DeleteResult } from "typeorm";
+import { SQLiteAppDataSource } from "../models/data-source-sqlite";
+import { User } from "../models/entities/User";
+import { ConflictError } from "../helpers/APIErrors";
+
+export default class UserService {
+    private readonly userRepository = SQLiteAppDataSource.getRepository(User);
+
+    async createUser(userData: User): Promise<User> {
+        const userExists = await this.getUserByEmail(userData.email);
+        
+        if (userExists) {
+            throw new ConflictError("User already exists");
+        };
+
+        const newUser = await this.userRepository.create(userData);
+        await this.userRepository.save(newUser);
+
+        return newUser;
+    };
+
+    async getAllUsers(): Promise<User[]> {
+        const users = await this.userRepository.find();
+        
+        return users;
+    };
+    
+    async getUserById(id: number): Promise<User | null> {
+        const user = await this.userRepository.findOneBy({ id });
+
+        return user || null;
+    };
+    
+    async getUserByEmail(email: string): Promise<User | null> {
+        const user = await this.userRepository.findOneBy({ email });
+
+        return user || null;
+    };
+
+    async updateUser(userData: Partial<User>, user: User): Promise<User | null> {
+        const updateUser = await this.userRepository.merge(user, userData);
+        await this.userRepository.save(updateUser);
+
+        return updateUser;
+    };
+
+    async deleteUser(id: number): Promise<DeleteResult> {
+        return await this.userRepository.delete(id);
+    };
+};
