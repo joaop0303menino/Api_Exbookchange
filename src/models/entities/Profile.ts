db1e9f41b64bef873c4ae80cdb9e4a8ff8201be6
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2025-06-20 13:14:01 -0300

    refactor(Profile): I added the relationship with Review

--- src/models/entities/Profile.ts
+++ src/models/entities/Profile.ts
@@ -1,16 +1,25 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
-@Entity("Profile")
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, OneToMany, JoinColumn } from "typeorm";
+import type { User } from "./User.ts";
+import { User as UserEntity } from "./User.ts";
+import { Review } from "./Review.ts";
+@Entity("profile")
 export class Profile {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({type: "varchar"})
-    nickname: string;
+  @OneToOne(() => UserEntity)
+  @JoinColumn({ name: "id_user" })
+  user: User;
 
-    @Column({type: "text"})
-    description: string;
-    
-    @Column({type: "blob"})
-    photo: Buffer;
-}; 
+  @Column({ type: "varchar"})
+  nickname: string;
+
+  @Column({ type: "text" })
+  description: string;
+
+  @Column({ type: "blob" })
+  photo: Buffer;
+
+  @OneToMany(() => Review, () => Review)
+  reviews: Review[];
+}
