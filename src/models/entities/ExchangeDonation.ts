1997d8f6d0280530bd8a1748fb982f44f2f7df14
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2025-06-20 13:27:51 -0300

    refactor(ExchangeDonation): I added the relationship with User and Announce

--- src/models/entities/ExchangeDonation.ts
+++ src/models/entities/ExchangeDonation.ts
@@ -1,10 +1,26 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToOne, OneToMany, JoinColumn} from "typeorm";
+import type { User } from "./User.ts";
+import { User as UserEntity } from "./User.ts";
+import type { Announce } from "./Announce.ts";
+import { Announce as AnnounceEntity } from "./Announce.ts";
+import { ExchangeDonationHistoric } from "./ExchangeDonationHistoric.ts";
 
-@Entity("ExchangeDonation")
+@Entity("exchange_donation")
 export class ExchangeDonation {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({type: "varchar"})
-    phone_user_receiver: string;
-};
+  @ManyToOne(() => UserEntity)
+  @JoinColumn({ name: "id_user" })
+  user: User;
+
+  @ManyToOne(() => AnnounceEntity)
+  @JoinColumn({ name: "id_announce" })
+  announce: Announce;
+
+  @Column({ type: "varchar"})
+  phone_user_receiver: string;
+
+  @OneToMany(() => ExchangeDonationHistoric, hist => hist.exchangeDonation)
+  history: ExchangeDonationHistoric[];
+}
