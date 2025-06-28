0fd57f6e6782c6d0991eb153d81a550c9402e229
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2025-06-27 23:32:59 -0300

    feat: AuthService with tests

--- tests/unitary/services/AuthService.test.ts
+++ tests/unitary/services/AuthService.test.ts
@@ -1,34 +1,63 @@
 import APIErrorsHandler from "../../../src/helpers/APIErrors";
+import { SQLiteAppDataSource } from "../../../src/models/data-source-sqlite";
 import { User } from "../../../src/models/entities/User";
-import AuthService from "../../../src/services/AuthService";
+import { AuthService } from "../../../src/services/AuthService";
+import { CacheService } from "../../../src/services/CacheService";
+import Crypt from "../../../src/services/Crypt";
+import UserService from "../../../src/services/UserService";
 
-describe("JWT tests", () => {
-    test("Should return token jwt string", async () => {
-        const user = new User();
-        user.id = 1;
+describe("AuthService tests", () => {
+    let userService: UserService;
+    let authService: AuthService;
+    let cacheService: CacheService;
+    let accessTokenToTests: string; 
+    let refreshTokenToTests: string; 
 
-        const token = await AuthService.generateToken(user.id);
-        
-        expect(token).not.toBe(1);
-        expect(typeof token).toBe("string");
+    beforeAll(async () => {
+        await SQLiteAppDataSource.initialize();
+        userService = new UserService();
+        authService = new AuthService();
+        cacheService = new CacheService();
     });
-    
-    test("Should verify token", async () => {
+
+    test("Should login user and return access token and refresh token hash", async () => {
         const user = new User();
-        user.id = 1;
+
+        const password = "123456";
+
+        const passwordHash = await Crypt.encrypt(password)
+
+        user.full_name = "Jhon Doe";
+        user.date_birth = new Date("1990/01/01");
+        user.email = "test9@test.com";
+        user.password = passwordHash;
+        user.phone = "1234567890";
         
-        const token = await AuthService.generateToken(user.id);
+        const newUser = await userService.createUser(user);
 
-        const validate1 = await AuthService.verifyToken(token)
-        .then(decode => {return decode})
-        .catch(err => {return err})
-        
-        const validate2 = await AuthService.verifyToken("should.return.false-hdsa")
-        .then(decode => {return decode})
-        .catch(err => {return err})
+        if (!newUser) {
+            throw new APIErrorsHandler("User creation failed", 500, newUser);
+        };
 
+        const {accessToken, refreshTokenHash} = await authService.login(user.email, password);
 
-        expect(validate1).not.toBeInstanceOf(APIErrorsHandler);
-        expect(validate2).toBeInstanceOf(APIErrorsHandler);
+        expect(accessToken).toBeDefined();
+        expect(refreshTokenHash).toBeDefined();
+
+        accessTokenToTests = accessToken;
+        refreshTokenToTests = refreshTokenHash;
+    });
+
+    test("Should validate user with access token", async () => {
+        const validate = await authService.accessToken(accessTokenToTests);
+
+        expect(validate).toBeTruthy();
+    });
+
+    test("Should login user with refresh token, should return new access and refresh token hashes", async () => {
+        const {accessToken, refreshTokenHash} = await authService.refreshToken(refreshTokenToTests);
+
+        expect(accessToken).toBeDefined();
+        expect(refreshTokenHash).toBeDefined();
     });
 });
