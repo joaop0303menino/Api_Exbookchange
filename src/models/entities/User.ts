e4dc9a56049dd332b247320c7ccf8a1a44b73d38
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2025-07-17 18:23:21 -0300

    feat: create table users

--- src/models/entities/User.ts
+++ src/models/entities/User.ts
@@ -1,28 +1,68 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToOne, OneToMany } from "typeorm";
+import { Profile } from "./Profile.ts";
+import { UserSetting } from "./User_setting.ts";
+import { Announce } from "./Announce.ts";
+import { ExchangeDonation } from "./ExchangeDonation.ts";
+import { Notification } from "./Notification.ts";
+import { UserPreference } from "./User_preference.ts";
+import { Complaint } from "./Complaint.ts";
 
-@Entity("users")
+@Entity("user")
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({type: "varchar"})
-    full_name: string;
+  @Column({ type: "varchar" })
+  full_name: string;
 
-    @Column({type: "date"})
-    date_birth: Date;
-    
-    @Column({type: "varchar", unique: true})
-    email: string;
-    
-    @Column({type: "varchar"})
-    password: string;
-    
-    @Column({ type: "varchar", nullable: true})
-    phone?: string;
+  @Column({ type: "date" })
+  date_birth: Date;
 
-    @CreateDateColumn({type: "datetime"})
-    created_at: Date;
-    
-    @UpdateDateColumn({type: "datetime"})
-    updated_at: Date;
-};
+  @Column({ type: "varchar", unique: true })
+  email: string;
+
+  @Column({ type: "varchar"})
+  password: string;
+
+  @Column({ type: "varchar"})
+  phone: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @Column({ 
+    type: "tinyint",
+    default: 0 
+  })
+  trust_seal: number;
+
+  @Column({ 
+    type: "tinyint",
+    default: 0 
+  })
+  blocked_user: number;
+
+  @OneToOne(() => Profile, profile => profile.user)
+  profile: Profile;
+
+  @OneToOne(() => UserSetting, setting => setting.user)
+  setting: UserSetting;
+
+  @OneToMany(() => Announce, announce => announce.user)
+  announces: Announce[];
+
+  @OneToMany(() => ExchangeDonation, ed => ed.user)
+  exchangeDonations: ExchangeDonation[];
+
+  @OneToMany(() => Notification, Notification => Notification.User)
+  Notifications: Notification[];
+
+  @OneToMany(() => UserPreference, up => up.user)
+  UserPreferences: UserPreference[];
+
+  @OneToMany(() => Complaint, Complaint => Complaint.user)
+  Complaints: Complaint[];
+}
