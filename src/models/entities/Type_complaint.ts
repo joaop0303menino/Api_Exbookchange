c735b9ca9cd00a3c4c6f6b7aefd6733b53f4e9cb
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2025-07-17 18:49:15 -0300

    refactor(TypeComplaint): I added the relationship with Complaint

--- src/models/entities/Type_complaint.ts
+++ src/models/entities/Type_complaint.ts
@@ -1,14 +1,17 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne} from "typeorm";
-import { Complaint } from "./Complaint.ts";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn} from "typeorm";
+import type { Complaint } from "./Complaint.ts";
+import { Complaint as ComplaintEntity} from "./Complaint.ts";
 
 @Entity("Type_complaint")
 export class TypeComplaint {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ManyToOne(() => ComplaintEntity)
+  @JoinColumn({ name: "id_complaint" })
+  Complaint: Complaint;
+
   @Column({ type: "varchar" })
   name: string;
 
-  @ManyToOne(() => Complaint, () => Complaint)
-  Complaints: Complaint[];
 }
