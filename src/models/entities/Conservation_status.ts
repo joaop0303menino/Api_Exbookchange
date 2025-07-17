d3e39e2ce6311756f8c38db35d11ddd9a66471ec
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2025-07-17 15:43:29 -0300

    feat: create table Conservacao_Status

---
+++ src/models/entities/Conservation_status.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import { Announce } from "./Announce.ts";
+
+export enum ConservacaoStatusEnum {
+  PESSIMO = 1,
+  RUIM = 2,
+  REGULAR = 3,
+  BOM = 4,
+  OTIMO = 5
+}
+
+@Entity("conservation_status")
+export class ConservationStatus {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "enum",
+    enum: ConservacaoStatusEnum,
+    default: ConservacaoStatusEnum.OTIMO
+  })
+  status: number;
+
+  @Column({ type: "text" })
+  description_status: string;
+
+  @OneToMany(() => Announce, announce => announce.conservationStatus)
+  announces: Announce[];
+}
