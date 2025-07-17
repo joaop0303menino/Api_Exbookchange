f7b5a3870833a5ad0ad984869b3161579155c861
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2025-07-17 12:48:37 -0300

    feat: create table Profile

---
+++ src/models/entities/Profile.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, OneToMany, JoinColumn } from "typeorm";
+import type { User } from "./User.ts";
+import { User as UserEntity } from "./User.ts";
+import { Review } from "./Review.ts";
+
+@Entity("profile")
+export class Profile {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToOne(() => UserEntity)
+  @JoinColumn({ name: "id_user" })
+  user: User;
+
+  @Column({ type: "varchar"})
+  nickname: string;
+
+  @Column({ type: "text" })
+  description: string;
+
+  @Column({ type: "blob" })
+  photo: Buffer;
+
+  @OneToMany(() => Review, () => Review)
+  reviews: Review[];
+}
