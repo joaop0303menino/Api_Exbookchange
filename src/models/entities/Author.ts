9e883dce10b3596ea315b01ddb1fbe694e52c48a
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2025-07-22 13:06:32 -0300

    refactor: fixed import UserPreference

--- src/models/entities/Author.ts
+++ src/models/entities/Author.ts
@@ -1,18 +1,18 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 import { Announce } from "./Announce";
-import { UserPreference } from "./User_preference";
+import { UserPreference } from "./UserPreference";
 
 @Entity("author")
 export class Author {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: "varchar"})
+  @Column({ type: "varchar" })
   full_name: string;
 
-  @OneToMany(() => Announce, announce => announce.author)
+  @OneToMany(() => Announce, (announce) => announce.author)
   announces: Announce[];
 
-  @OneToMany(() => UserPreference, UserPreference => UserPreference.author)
+  @OneToMany(() => UserPreference, (UserPreference) => UserPreference.author)
   UserPreferences: UserPreference[];
 }
